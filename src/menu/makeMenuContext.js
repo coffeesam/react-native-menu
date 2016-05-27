f6ebc8ec9af6357fee9607ad277bd1e51aadf8e3
Jia Rong Lim <jr.loves.maths@gmail.com>
2016-05-27 13:08:57 +0800

    Fix require wrong React

--- src/menu/makeMenuContext.js
+++ src/menu/makeMenuContext.js
@@ -56,7 +56,7 @@ module.exports = (React, ReactNative, { constants, model, styles }) => {
       return this.state.openedMenu
     },
     openMenu(name) {
-      const handle = React.findNodeHandle(this._menus[name].ref);
+      const handle = ReactNative.findNodeHandle(this._menus[name].ref);
       UIManager.measure(handle, (x, y, w, h, px, py) => {
         this._menus[name].measurements = { x, y, w, h, px, py };
 
